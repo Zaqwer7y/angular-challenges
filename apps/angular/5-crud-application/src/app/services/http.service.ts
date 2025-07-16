9301ea65d2ff4ff7fed580fe58b798218fbf2dee
Zaqwer7y <marin19@mail.bg>
2025-07-16 15:51:14 +0300

    5: error handling

--- apps/angular/5-crud-application/src/app/services/http.service.ts
+++ apps/angular/5-crud-application/src/app/services/http.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable, } from '@angular/core';
 import { Todo } from '../models/todo';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { randText } from '@ngneat/falso';
 
 @Injectable({
@@ -9,11 +9,11 @@ import { randText } from '@ngneat/falso';
 })
 export class HttpService {
   private http = inject(HttpClient);
-  
 
   getTodos() : Observable<Todo[]> {
     return this.http
-      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos');
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+      .pipe(catchError(x => this.handleError(x)));
   }
 
   update(todo: Todo) : Observable<Todo> {
@@ -26,10 +26,17 @@ export class HttpService {
                 'Content-type': 'application/json; charset=UTF-8',
               },
             },
-          )
+          ).pipe(catchError(x => this.handleError(x)));
   }
 
   delete(id: number) : Observable<Todo>{
-    return this.http.delete<Todo>(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    return this.http.delete<Todo>(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .pipe(catchError(x => this.handleError(x)));
+  }
+
+  handleError(error: HttpErrorResponse) : Observable<any>{
+    console.log(error);
+    alert(error.error);
+    return throwError(() => error);
   }
 }
