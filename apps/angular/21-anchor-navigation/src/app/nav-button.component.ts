58a24ae62cf0748501bc41d2540d7506749936b4
Marin Georgiev <maringeorgievmig@gmail.com>
2025-07-23 01:09:22 +0300

    21: routerLink and fragment scrolling

--- apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
+++ apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
@@ -1,17 +1,20 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'nav-button',
   template: `
-    <a [href]="href()">
+    <a [routerLink]="this.navRouterLink()" [fragment]="this.navFragment()">
       <ng-content />
     </a>
   `,
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterLink],
 })
 export class NavButtonComponent {
-  href = input('');
+  navRouterLink = input('');
+  navFragment = input<string | undefined>(undefined);
 }
